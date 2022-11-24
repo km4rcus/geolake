4b3362532d9918a70b001e557f319602385d04e8
Jakub Walczak <kubawalczak@gmail.com>
2022-11-24 14:57:07 +0100

    Use formatter for logging. Read from env var

--- web/app/meta.py
+++ web/app/meta.py
@@ -11,6 +11,15 @@ class LoggableMeta(type):
         # TODO: eventually, configure logging format
         res = super().__new__(cls, child_cls, bases, namespace)
         if hasattr(res, "_LOG"):
+            format_ = os.environ.get(
+                "LOGGING_FORMAT",
+                "%(asctime)s %(message)s",
+            )
+            formatter = logging.Formatter(
+                format_, datefmt="%m/%d/%Y %I:%M:%S %p"
+            )
             res._LOG.setLevel(os.environ.get("LOGGING_LEVEL", "INFO"))
-            res._LOG.addHandler(logging.StreamHandler())
+            stream_handler = logging.StreamHandler()
+            stream_handler.setFormatter(formatter)
+            res._LOG.addHandler(stream_handler)
         return res
